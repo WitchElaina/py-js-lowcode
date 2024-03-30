eab076c03a2024a2b23965793e634fe0677e1ee2
WitchElaina <msz13399173387@gmail.com>
2024-03-30 11:13:02 +0800

    ✨ feat(schema): Add type support for states

--- src/types/component.ts
+++ src/types/component.ts
@@ -6,10 +6,23 @@ export interface BaseVariable<T> {
   value?: T[keyof T];
 }
 
+// state 值类型
+export interface BaseState<T> {
+  // 在全局 state 管理器中的 id，未注册则为 null
+  globalId: string | null;
+  // config panel 中展示给用户的 label
+  label: string;
+  // 默认值，初始化时使用
+  defaultValue: T[keyof T];
+}
+
 export interface BaseCallback {
   name: string;
   label: string;
 }
+
+// 将自定义组件的 props 与 React 原生 Ref 属性合并
+type CombineProps<T> = T & React.RefAttributes<HTMLElement>;
 
 /**
  * BaseComponent interface.
@@ -25,11 +38,16 @@ export interface BaseComponent<ComponentType, PropsType> {
   // 组件的示例，用于组件库中展示缩略图
   example: ReactNode;
   // 组件属性，即 react 组件的 props
-  props?: PropsType & React.RefAttributes<HTMLElement>;
+  props?: CombineProps<PropsType>;
   // 用户可配置组件属性
-  variables: BaseVariable<PropsType & React.RefAttributes<HTMLElement>>[];
+  variables: CombineProps<BaseVariable<PropsType>>[];
   // 配置面板组件
   configPanel?: React.FC<{ schema: Schema }>;
   // 默认的 schema
   defaultSchema: Schema;
+  // 组件 state
+  states?: Record<
+    keyof CombineProps<PropsType>,
+    BaseState<CombineProps<PropsType>>
+  >;
 }
--- src/types/schema.ts
+++ src/types/schema.ts
@@ -1,12 +1,11 @@
 import { components } from '../components';
-import { BaseComponent } from './component';
 
 /**
  * 低代码工程文件类型
  * @description 用于描述一个低代码工程文件的结构
  */
 export interface Schema {
-  // 组件id
+  // 组件 id
   id?: string;
   // 组件名称
   componentNames: keyof typeof components;
@@ -15,9 +14,14 @@ export interface Schema {
   // 组件标题
   title: string;
   // 组件状态变量
-  states: Record<string, any>;
+  states: Record<
+    // state 属性名称
+    keyof (keyof (typeof components)[keyof typeof components]['states']),
+    // 全局 states 中对应的 id
+    string
+  >;
   // voidElementTag 是否是无子元素的标签
   voidElementTag?: boolean;
-  // 子组件schema
+  // 子组件 schema
   children: Schema[] | null;
 }
